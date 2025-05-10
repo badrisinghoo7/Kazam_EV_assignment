d7fc62fb588492890dabe901f48dced3584eac74
Badri vishal singh <badri.singh8090@gmail.com>
2025-05-10 14:12:16 +0530

    frontend update

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,2 @@
-// export const api ="https://kazam-assignment-cf6k.onrender.com";
-export const api ="http://localhost:3000";
+export const api ="https://kazam-ev-assignment.onrender.com";
+// export const api ="http://localhost:3000";
